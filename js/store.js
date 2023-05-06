4c6d883813a2746012f27eaabe8c431f4bd1b445
NICKY-100 <74373702+NICKY-100@users.noreply.github.com>
2023-05-06 16:46:20 +0100

    Update store.js

--- js/store.js
+++ js/store.js
@@ -82,11 +82,6 @@
 
 		// Generate an ID
 	    var newId = Date.now(); 
-// 	    var charset = "0123456789";
-
-//         for (var i = 0; i < 6; i++) {
-//      		newId += charset.charAt(Math.floor(Math.random() * charset.length));
-// 		}
 
 		// If an ID was actually given, find the item and update each property
 		if (id) {
