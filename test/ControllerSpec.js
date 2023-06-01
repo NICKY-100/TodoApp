af63417386d086f4eeb6282ae9bad70f7239cd5c
Nicky <geesin11@gamil.com>
2023-06-01 18:46:06 +0100

    should show completed entries

--- test/ControllerSpec.js
+++ test/ControllerSpec.js
@@ -92,6 +92,11 @@ describe('controller', function () {
 		});
 
 		it('should show completed entries', function () {
+			const todo = { title: 'my todo', completed: true };
+			setUpModel([todo])
+			subject.setView('#/completed')
+			expect(model.read).toHaveBeenCalledWith({ completed: true }, jasmine.any(Function));
+			expect(view.render).toHaveBeenCalledWith('showEntries', [todo])
 			// TODO: write test
 		});
 	});
